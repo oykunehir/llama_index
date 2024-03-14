fe12251bb67037ee89f6c0e66ce9c62e1b73b047
ahmadhajmosa <33151918+ahmadhajmosa@users.noreply.github.com>
2024-03-14 15:26:55 +0100

    Update base.py

--- llama_index/indices/knowledge_graph/base.py
+++ llama_index/indices/knowledge_graph/base.py
@@ -210,11 +210,10 @@ class KnowledgeGraphIndex(BaseIndex[KG]):
                     triplet_texts, show_progress=self._show_progress
                 )
                 vectors.append((f"vec{tr_i}", embed_outputs[0], {"subject": tr}))
+
             logger.debug(f"vectors: {vectors}")
             upsert_response = pincone_index.upsert(
-                    vectors=vectors,
-                    namespace="dexter"
-            )
+                    vectors=vectors)
             for triplet in triplets:
                 subj, _, obj = triplet
                 self.upsert_triplet(triplet)
